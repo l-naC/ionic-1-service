0876d76542f1721d3d432ec9dd5c4d35d97de079
lena clavier <lenaclavier@MacBook-Pro-de-lena.local>
2019-04-12 12:38:51 +0200

    Debut service. #2

--- src/app/tabs.service.ts
+++ src/app/tabs.service.ts
@@ -5,5 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class TabsService {
 
-  constructor() { }
+	constructor() { }
+
+	getTabBameForId(id){
+		public tab1 = "Tab 1";
+		public tab2 = "Tab 2";
+		public tab3 = "Tab 3";
+	}
+
+
 }
